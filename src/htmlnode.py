abec28d4a19ba1ac41d47471f4a3c41369961991
David Lashock Jr. <dlashock@gmail.com>
2024-12-26 16:01:51 -0500

    Finished HTMLNode and test cases

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,12 +1,21 @@
-class HTMLNODE:
+class HTMLNode:
     def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
         self.value = value
         self.children = children
         self.props = props
 
+    def __repr__(self):
+        return f'HTMLNode(Tag:{self.tag}, Value:{self.value}, Children:{self.children}, Props:{self.props})'
+                
     def to_html(self):
-        raise NotImplementedError
+        raise NotImplementedError("to_html method not implemented")
     
     def props_to_html(self):
+        result = ""
+
+        if self.props != None:
+            for key, value in self.props.items():
+                result += f' {key}="{value}"'
         
+        return result
--- src/main.py
+++ src/main.py
@@ -1,7 +1,16 @@
-from textnode import *
+from textnode import TextNode, TextType
+from htmlnode import HTMLNODE
 
 def main():
-    temp = TextNode("poop", TextType.BOLD, "https://boot.dev")
-    print(temp)
+    # temp = TextNode("poop", TextType.BOLD, "https://boot.dev")
+    # print(temp)
+    # props = {
+    #     "href": "https://www.google.com", 
+    #     "target": "_blank",
+    # }
+    # node = HTMLNode("p", "Test", "", props)
+    # print(node.to_html())
+    # print(' href="https://www.google.com" target="_blank"')
+    pass
 
 main()
