d4a710b525a5ac2c5675a510e830f17397530eb4
David Lashock Jr. <dlashock@gmail.com>
2024-12-26 22:43:40 -0500

    Added Leafnode class and tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -19,3 +19,23 @@ class HTMLNode:
                 result += f' {key}="{value}"'
         
         return result
+    
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, children=None, props=None):
+        if children != None:
+            raise ValueError("LeafNode cannot have children")
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value == None:
+            raise ValueError("All leaf node must have a value")
+        if self.tag == None:
+            return self.value
+        if self.props == None:
+            return f"<{self.tag}>{self.value}</{self.tag}>"
+
+        props_string = ""
+        for key, value in self.props.items():
+            props_string += f' {key}="{value}"'
+
+        return f"<{self.tag}{props_string}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -50,5 +50,62 @@ class TestHTMLNode(unittest.TestCase):
             "HTMLNode(Tag:p, Value:What a strange world, Children:None, Props:{'class': 'primary'})",
         )
 
+    def test_leafnode_values(self):
+        node = LeafNode(
+            "p",
+            "What a strange world",
+            None,
+            {"href": "https://boot.dev"}
+        )
+        self.assertEqual(
+            node.tag,
+            "p"
+        )
+        self.assertEqual(
+            node.value,
+            "What a strange world",
+        )
+        self.assertEqual(
+            node.props,
+            {"href": "https://boot.dev"}
+        )
+
+    def test_leafnode_to_html_1(self): #testing a single prop
+        node = LeafNode(
+            "a",
+            "What a strange world",
+            None,
+            {"href": "https://boot.dev"}
+        )
+        self.assertEqual(
+            node.to_html(),
+            '<a href="https://boot.dev">What a strange world</a>',
+        )
+
+    def test_leafnode_to_html_2(self): #testing 2 props
+        node = LeafNode(
+            "a",
+            "What a strange world",
+            None,
+            {"href": "https://boot.dev",
+             "target": "_blank"}
+        )
+        self.assertEqual(
+            node.to_html(),
+            '<a href="https://boot.dev" target="_blank">What a strange world</a>',
+        )
+
+    def test_leafnode_repr(self):
+        node = LeafNode(
+            "p",
+            "What a strange world",
+            None,
+            {"class": "primary"},
+        )
+        self.assertEqual(
+            node.__repr__(),
+            "HTMLNode(Tag:p, Value:What a strange world, Children:None, Props:{'class': 'primary'})",
+        )
+    
 if __name__ == "__main__":
     unittest.main()
